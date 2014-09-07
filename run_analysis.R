334eda6331b831e5fe50e5c7c02eba0a6e120bda
Jeff Shilling <Jeff@shilling.us.com>
2014-09-07 14:02:29 -0400

    initial file

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+## This script creates a tidy data set from data found in the UCI HAR Dataset
+## 
+##    1. Merge training and test sets from Samsung Galaxy S accelerometers to create one data set.
+##    2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+##    3. Applies descriptive activity names to name the activities in the data set
+##    4. Labels the data set with descriptive variable names. 
+##    5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
