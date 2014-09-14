70a220f86f822c6b77ee7e977b9cae8af803509c
Jeff Shilling <jeff@shilling.us.com>
2014-09-14 17:33:06 -0400

    Working copy up to creating the tidy date.

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,51 @@
-## This script creates a tidy data set from data found in the UCI HAR Dataset
-## 
-##    1. Merge training and test sets from Samsung Galaxy S accelerometers to create one data set.
-##    2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-##    3. Applies descriptive activity names to name the activities in the data set
-##    4. Labels the data set with descriptive variable names. 
-##    5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## Go to top level directory
+setwd("C:/Users/IBM_ADMIN/Dropbox/R Projects/DataCleaningProject/UCI HAR Dataset")
+
+
+createFrame <- function(label, good) {
+    ## function to read the known files
+    ## pass in the directory name
+    ## return the datafrome with the correct variables
+    
+    ## use the label to create a path
+    path <- paste("./", label, "/", sep="")
+    
+    ## build the name of each target file and read it with 
+    ## the right column names
+    ##
+    ## get the subject identifiers
+    f <- paste(path, "subject_",label,".txt", sep="")
+    subject <- read.table(f, col.names = c("subject"))
+    
+    ## get all the readings (x values) and label them with the features labels
+    f <- paste(path, "x_",label,".txt", sep="")
+    x <- read.table(f, col.names = features$variable)
+    x <- x[,good]
+    ## get order in which the activities were performed (y)
+    f <- paste(path, "y_",label,".txt", sep="")
+    y <- read.table(f, col.names = c("activity"))
+    
+    # turn y column into factors of activity labels
+    y[,1] <- factor(y$activity, labels = act_lab$activity)
+    # bind subject column and activity labels to features
+    return(cbind(subject, y, x))
+}
+
+## grab the descriptive info
+
+act_lab <- read.table("activity_labels.txt", col.names = c("i","activity"))
+features <- read.table("features.txt", col.names = c("i", "variable"))
+## identify the columns we want to end up with 
+## which are the ones for mean and standard deviation
+## then apply them (along with the subject and activity columns)
+good <- grep("mean()|std()", features[,2])
+
+## read the test and train data into data frames
+test <- createFrame("test", good)
+train <- createFrame("train", good)
+## merge the two data sets
+activityData <- rbind(test,train)
+rm(test,train)  # free up some RAM
+
+
+
